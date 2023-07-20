0484af11ea4b7c82666bed028e02530567696232
APANA49 <aungphonenaing57@gmail.com>
2023-07-20 17:40:37 +0800

    Update user.ts schema, making fields non-nullable and default value for timestamps.

--- backend/src/modules/authentication/user.ts
+++ backend/src/modules/authentication/user.ts
@@ -2,9 +2,9 @@ import { serial, text, timestamp, pgTable, uuid } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
     id: uuid("id").defaultRandom().primaryKey(),
-    name: text("name"),
-    email: text("email"),
-    password: text("password"),
-    createdAt: timestamp("created_at"),
-    updatedAt: timestamp("updated_at"),
+    name: text("name").notNull(),
+    email: text("email").notNull(),
+    password: text("password").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
   });
