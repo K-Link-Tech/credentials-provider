6e650f3df326fed7a48bfdddb6a790aff2275680
APANA49 <aungphonenaing57@gmail.com>
2023-07-26 11:51:31 +0800

    Installs and creates dotenv file

--- backend/src/modules/authentication/index.ts
+++ backend/src/modules/authentication/index.ts
@@ -1,12 +1,16 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { sql } from "drizzle-orm";
 import { users } from "./schema/users.schema";
-import { express } from "express";
+import express, { json } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import postgres from "postgres";
  
 const connectionString = "postgres://postgres:Micahsim00**@localhost:5432/credential_provider"
 const credentialProviderPG = postgres(connectionString, { max: 1 })
 const db = drizzle(credentialProviderPG);
 
+const app = express();
 
+app.listen()
 
