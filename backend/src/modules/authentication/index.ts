0f02a17b8dd624d8be64300b975a0656d82be2be
APANA49 <aungphonenaing57@gmail.com>
2023-07-26 11:29:22 +0800

    Installs cors and typescript types for it

--- backend/src/modules/authentication/index.ts
+++ backend/src/modules/authentication/index.ts
@@ -1,11 +1,12 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { sql } from "drizzle-orm";
 import { users } from "./schema/users.schema";
+import { express } from "express";
 import postgres from "postgres";
  
 const connectionString = "postgres://postgres:Micahsim00**@localhost:5432/credential_provider"
 const credentialProviderPG = postgres(connectionString, { max: 1 })
 const db = drizzle(credentialProviderPG);
- 
-console.log(db.select().from(users));
+
+
 
